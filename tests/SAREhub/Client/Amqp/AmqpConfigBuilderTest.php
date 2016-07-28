0c4d5a9c6a91930be5c911e9f0845c158842f66d
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-07-28 16:48:12 +0200

    added AmqpConfigBuilder class

---
+++ tests/SAREhub/Client/Amqp/AmqpConfigBuilderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace SAREhub\Client\Amqp;
+
+use PHPUnit\Framework\TestCase;
+
+class AmqpConfigBuilderTest extends TestCase {
+	
+	
+	public function testBuild() {
+		$config = [
+		  'host' => 'host123',
+		  'port' => 10000,
+		  'username' => 'user',
+		  'password' => 'pass',
+		  'vhost' => 'vhost123'
+		];
+		
+		$this->assertEquals($config, (new AmqpConfigBuilder())
+		  ->host($config['host'])
+		  ->port($config['port'])
+		  ->username($config['username'])
+		  ->password($config['password'])
+		  ->vhost($config['vhost'])
+		  ->build()->getAll()
+		);
+	}
+}
