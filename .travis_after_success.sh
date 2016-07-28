367b1026f76981336fbb58f23783200e234837b5
a.wasiak@sare.pl <a.wasiak@sare.pl>
2016-07-28 14:01:06 +0200

    added travis

---
+++ .travis_after_success.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "SAREhub/PHP_Client" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
+
+  echo -e "Publishing PHPDoc...\n"
+  cp -R build/docs $HOME/docs-latest
+
+  cd $HOME
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null
+
+  cd PHP_Commons
+
+  git rm -rf ./docs/$TRAVIS_BRANCH
+
+  mkdir docs
+  cd docs
+  mkdir $TRAVIS_BRANCH
+
+  cp -Rf $HOME/docs-latest/* ./$TRAVIS_BRANCH/
+
+  git add -f .
+  git commit -m "PHPDocumentor (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
+  git push -fq origin gh-pages > /dev/null
+  echo -e "Published PHPDoc to gh-pages.\n"
+fi
---
+++ README.md
@@ -0,0 +1,13 @@
+# SAREhub PHP Client 
+[![Coding Standards](https://img.shields.io/badge/cs-PSR--2--R-yellow.svg)](https://github.com/php-fig-rectified/fig-rectified-standards) 
+[![Build Status](https://travis-ci.org/SAREhub/PHP_Client.svg?branch=master)](https://travis-ci.org/SAREhub/PHP_Client)
+[![Code Climate](https://codeclimate.com/github/SAREhub/PHP_Client/badges/gpa.svg)](https://codeclimate.com/github/SAREhub/PHP_Client)
+[![Test Coverage](https://codeclimate.com/github/SAREhub/PHP_Client/badges/coverage.svg)](https://codeclimate.com/github/SAREhub/PHP_Client/coverage)
+
+### What is this repository for? ###
+* This project contains client code for connecte to SAREhub event bus
+
+### How do I get set up? ###
+```
+composer require sarehub/client
+```
