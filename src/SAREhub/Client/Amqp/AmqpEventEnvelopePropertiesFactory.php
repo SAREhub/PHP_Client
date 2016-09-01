67e7eb3766ef8bd8cb865dc40b54be35825c5ded
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-09-01 13:31:01 +0200

    fixed hinting for method create

--- src/SAREhub/Client/Amqp/AmqpEventEnvelopePropertiesFactory.php
+++ src/SAREhub/Client/Amqp/AmqpEventEnvelopePropertiesFactory.php
@@ -2,9 +2,11 @@
 
 namespace SAREhub\Client\Amqp;
 
+use PhpAmqpLib\Message\AMQPMessage;
+
 class AmqpEventEnvelopePropertiesFactory {
 	
-	public function createFromMessage($amqpMessage) {
+	public function createFromMessage(AMQPMessage $amqpMessage) {
 		$properties = new AmqpEventEnvelopeProperties();
 		
 		$deliveryInfo = $amqpMessage->delivery_info;
@@ -15,7 +17,6 @@ class AmqpEventEnvelopePropertiesFactory {
 		$properties->setReplyTo(isset($messageProperties['reply_to']) ? $messageProperties['reply_to'] : '');
 		$properties->setCorrelationId(isset($messageProperties['correlation_id']) ? $messageProperties['correlation_id'] : '');
 		$properties->setPriority(isset($messageProperties['priority']) ? $messageProperties['priority'] : 0);
-		
 		
 		return $properties;
 	}
