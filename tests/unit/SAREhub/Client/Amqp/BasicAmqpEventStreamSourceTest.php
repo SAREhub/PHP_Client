1f0af2fd528cd65bf8416565114cc03ce3fc0ab6
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-10-24 13:52:43 +0200

    fix nonblocking channel wait and refactored tests for Amqp source

--- tests/unit/SAREhub/Client/Amqp/BasicAmqpEventStreamSourceTest.php
+++ tests/unit/SAREhub/Client/Amqp/BasicAmqpEventStreamSourceTest.php
@@ -4,11 +4,15 @@ namespace SAREhub\Client\Amqp;
 
 use GuzzleHttp\Promise\CancellationException;
 use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Wire\IO\SocketIO;
 use PHPUnit\Framework\TestCase;
 use SAREhub\Client\Event\BasicEventEnvelope;
 
 class BasicAmqpEventStreamSourceTest extends TestCase {
 	
+	private $socketMock;
+	private $connectionMock;
 	private $channelMock;
 	private $consumerBuilderMock;
 	private $consumerMock;
@@ -16,95 +20,115 @@ class BasicAmqpEventStreamSourceTest extends TestCase {
 	private $sourceMock;
 	private $eventEnvelopeMock;
 	
+	/**
+	 * @var BasicAmqpEventStreamSource
+	 */
+	private $source;
+	
 	protected function setUp() {
-		$this->channelMock = $this->getMockBuilder(AMQPChannel::class)
-		  ->disableOriginalConstructor()
-		  ->getMock();
+		$this->channelMock = $this->createMock(AMQPChannel::class);
+		$this->connectionMock = $this->createMock(AMQPStreamConnection::class);
+		$this->channelMock->method('getConnection')->willReturn($this->connectionMock);
+		$this->socketMock = $this->createMock(SocketIO::class);
+		$this->connectionMock->method('getSocket')->willReturn($this->socketMock);
 		
-		$this->consumerBuilderMock = $this->getMockBuilder(AmqpEventConsumerBuilder::class)->getMock();
-		$this->consumerMock = $this->getMockBuilder(\stdClass::class)->setMethods(['__invoke'])->getMock();
+		$this->consumerBuilderMock = $this->createMock(AmqpEventConsumerBuilder::class);
+		$this->consumerMock = $this->createPartialMock(\stdClass::class, ['__invoke']);
 		$this->consumerBuilderMock->method('build')->willReturn($this->consumerMock);
 		
-		$this->sourceMock = $this->getMockBuilder(BasicAmqpEventStreamSource::class)
-		  ->disableOriginalConstructor()
-		  ->getMock();
-		
+		$this->sourceMock = $this->createMock(BasicAmqpEventStreamSource::class);
 		$this->sourceMock->method('getChannel')->willReturn($this->channelMock);
 		
-		$this->eventEnvelopeMock = $this->getMockBuilder(BasicEventEnvelope::class)
-		  ->disableOriginalConstructor()
-		  ->getMock();
+		$this->eventEnvelopeMock = $this->createMock(BasicEventEnvelope::class);
+		
+		$this->source = $source = new BasicAmqpEventStreamSource($this->channelMock, [
+		  'queueName' => 'test',
+		  'consumerBuilder' => $this->consumerBuilderMock
+		]);
 	}
 	
-	public function testCreate() {
+	public function testCreateThenConsumerBuilderCallSource() {
 		$this->consumerBuilderMock->expects($this->once())->method('source');
+		new BasicAmqpEventStreamSource($this->channelMock, [
+		  'queueName' => 'test',
+		  'consumerBuilder' => $this->consumerBuilderMock
+		]);
+	}
+	
+	public function testCreateThenQueueSets() {
+		$source = new BasicAmqpEventStreamSource($this->channelMock, [
+		  'queueName' => 'test',
+		  'consumerBuilder' => $this->consumerBuilderMock
+		]);
+		
+		$this->assertEquals('test', $source->getQueue());
+	}
+	
+	public function testCreateWithConsumerTagThenConsumerTagSets() {
 		$source = new BasicAmqpEventStreamSource($this->channelMock, [
 		  'queueName' => 'test',
 		  'consumerTag' => 'tag',
 		  'consumerBuilder' => $this->consumerBuilderMock
 		]);
-		
-		$this->assertEquals('test', $source->getQueue());
 		$this->assertEquals('tag', $source->getConsumerTag());
 	}
 	
-	public function testFlowOpen() {
-		$source = new BasicAmqpEventStreamSource($this->channelMock, [
-		  'queueName' => 'test',
-		  'consumerBuilder' => $this->consumerBuilderMock
-		]);
-		
-		$source->flow();
-		$this->assertInstanceOf(\Generator::class, $source->getFlowControl());
-		$this->assertTrue($source->isInFlowMode());
+	public function testFlowOpenThenGetFlowControlWillReturnGeneratorInstance() {
+		$this->source->flow();
+		$this->assertInstanceOf(\Generator::class, $this->source->getFlowControl());
 	}
 	
-	public function testFlow() {
-		$this->channelMock->expects($this->once())
-		  ->method('basic_consume')
+	public function testFlowOpenThenIsInFlowModeReturnTrue() {
+		$this->source->flow();
+		$this->assertTrue($this->source->isInFlowMode());
+	}
+	
+	public function testFlowControlNextWhenFirstTimeThenBasicConsume() {
+		$this->channelMock->expects($this->once())->method('basic_consume')
 		  ->with('test', '', false, false, false, false, $this->identicalTo($this->consumerMock));
-		
+		$this->source->flow();
+		$this->source->getFlowControl()->next();
+	}
+	
+	public function testFlowControlNextWhenNextCallTimeThenNotBasicConsume() {
+		$this->source->flow();
+		$this->source->getFlowControl()->next();
+		$this->channelMock->expects($this->never())->method('basic_consume');
+		$this->source->getFlowControl()->next();
+	}
+	
+	public function testFlowControlNextThenChannelWait() {
+		$this->socketMock->method('select')->willReturn(1);
 		$this->channelMock->expects($this->once())->method('wait')
 		  ->with(null, true, BasicAmqpEventStreamSource::DEFAULT_TIMEOUT);
 		$this->channelMock->callbacks = [1]; // needs for count in flow loop
-		$source = new BasicAmqpEventStreamSource($this->channelMock, [
-		  'queueName' => 'test',
-		  'consumerBuilder' => $this->consumerBuilderMock
-		]);
-		
-		$source->flow();
-		$source->getFlowControl()->next();
+		$this->source->flow();
+		$this->source->getFlowControl()->next();
 	}
 	
-	public function testStopFlow() {
-		$this->channelMock->expects($this->once())->method('basic_cancel')->with('');
-		$this->channelMock->expects($this->once())->method('wait');
-		
+	public function testFlowControlNextWhenSocketSelectReturnZeroThenNotWait() {
+		$this->channelMock->expects($this->never())->method('wait');
 		$this->channelMock->callbacks = [1]; // needs for count in flow loop
-		$source = new BasicAmqpEventStreamSource($this->channelMock, [
-		  'queueName' => 'test',
-		  'consumerBuilder' => $this->consumerBuilderMock
-		]);
-		
-		$source->flow();
-		$flowControl = $source->getFlowControl();
-		$flowControl->next();
-		$source->stopFlow();
-		$this->assertFalse($source->isInFlowMode());
-		$flowControl->next();
+		$this->socketMock->method('select')->willReturn(0);
+		$this->source->flow();
+		$this->source->getFlowControl()->next();
 	}
 	
-	/**
-	 * @expectedException \SAREhub\Client\Amqp\AmqpException
-	 */
+	public function testStopFlowWhenFlowThenIsNotInFlowModeReturnFalse() {
+		$this->source->flow();
+		$this->source->stopFlow();
+	}
+	
+	public function testStopFlowThenChannelCancel() {
+		$this->channelMock->expects($this->once())->method('basic_cancel')->with('');
+		$this->source->flow();
+		$this->source->stopFlow();
+	}
+	
 	public function testFlowOpenWhenOpened() {
-		$source = new BasicAmqpEventStreamSource($this->channelMock, [
-		  'queueName' => 'test',
-		  'consumerBuilder' => $this->consumerBuilderMock
-		]);
-		
-		$source->flow();
-		$source->flow();
+		$this->expectException(AmqpException::class);
+		$this->source->flow();
+		$this->source->flow();
 	}
 	
 	public function testDefaultProcessPromiseFactoryForResolve() {
