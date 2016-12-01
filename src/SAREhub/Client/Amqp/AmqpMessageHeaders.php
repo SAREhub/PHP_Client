a32fa582a5309ccac1807eb462c8f42a57eca996
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-12-01 13:56:00 +0100

    added helper methods

--- src/SAREhub/Client/Amqp/AmqpMessageHeaders.php
+++ src/SAREhub/Client/Amqp/AmqpMessageHeaders.php
@@ -18,4 +18,43 @@ class AmqpMessageHeaders {
 	const REPLY_TO = 'amqp_reply_to';
 	const CORRELATION_ID = 'amqp_correlation_id';
 	const PRIORITY = 'amqp_priority';
+	
+	private static $headers = [
+	  self::CONSUMER_TAG,
+	  self::DELIVERY_TAG,
+	  self::REDELIVERED,
+	  self::EXCHANGE,
+	  self::ROUTING_KEY,
+	  self::CONTENT_TYPE,
+	  self::CONTENT_ENCODING,
+	  self::REPLY_TO,
+	  self::CORRELATION_ID,
+	  self::PRIORITY
+	];
+	
+	private static $propertyHeaders = [
+	  self::CONTENT_TYPE,
+	  self::CONTENT_ENCODING,
+	  self::REPLY_TO,
+	  self::CORRELATION_ID,
+	  self::PRIORITY
+	];
+	
+	public static function getMessagePropertyName($header) {
+		return substr($header, 5);
+	}
+	
+	/**
+	 * @return array
+	 */
+	public static function getAll() {
+		return self::$headers;
+	}
+	
+	/**
+	 * @return array
+	 */
+	public static function getPropertyHeaders() {
+		return self::$propertyHeaders;
+	}
 }
