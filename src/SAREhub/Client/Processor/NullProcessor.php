5ad61acf607a7f108046abebaa70dab7a4075d80
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-12-08 11:39:47 +0100

    added NullProcessor

---
+++ src/SAREhub/Client/Processor/NullProcessor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SAREhub\Client\Processor;
+
+use SAREhub\Client\Message\Exchange;
+
+/**
+ * Represents "dev/null" processor.
+ */
+class NullProcessor implements Processor {
+	
+	public function process(Exchange $exchange) {
+		
+	}
+}
