faf60f97b71d25a5cde8461a913030173d3f443b
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-12-12 11:41:50 +0100

    added HeaderAppenderProcessor

---
+++ src/SAREhub/Client/Processor/HeaderAppenderProcessor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SAREhub\Client\Processor;
+
+use SAREhub\Client\Message\Exchange;
+
+/**
+ * Appends defined headers to input message.
+ */
+class HeaderAppenderProcessor implements Processor {
+	
+	private $headers = [];
+	
+	public static function newInstance() {
+		return new self();
+	}
+	
+	/**
+	 * @param array $headers
+	 * @return $this
+	 */
+	public function withHeaders(array $headers) {
+		$this->headers = $headers;
+		return $this;
+	}
+	
+	public function process(Exchange $exchange) {
+		$in = $exchange->getIn();
+		foreach ($this->getHeaders() as $header => $value) {
+			$in->setHeader($header, $value);
+		}
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getHeaders() {
+		return $this->headers;
+	}
+}
---
+++ tests/unit/SAREhub/Client/Processor/HeaderAppenderProcessorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SAREhub\Client\Processor;
+
+use PHPUnit\Framework\TestCase;
+use SAREhub\Client\Message\BasicExchange;
+use SAREhub\Client\Message\BasicMessage;
+
+class HeaderAppenderProcessorTest extends TestCase {
+	
+	public function testProcessThenMessageHeadersAppended() {
+		$message = BasicMessage::newInstance()->setHeader('a', 1);
+		$startHeaders = $message->getHeaders();
+		
+		$processor = HeaderAppenderProcessor::newInstance()
+		  ->withHeaders(['b' => 2, 'c' => 3]);
+		$processor->process(BasicExchange::newInstance()->setIn($message));
+		$this->assertEquals($startHeaders + $processor->getHeaders(), $message->getHeaders());
+	}
+}
