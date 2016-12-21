b37ae3f61be8dee29d094f3564a7086a3e162f5a
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-12-21 11:21:16 +0100

    clean

--- src/SAREhub/Client/Event/EventDeserializeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace SAREhub\Client\Event;
-
-
-class EventDeserializeException extends \Exception {
-	
-}
--- src/SAREhub/Client/Event/EventSerializeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace SAREhub\Client\Event;
-
-
-class EventSerializeException extends \Exception {
-	
-}
