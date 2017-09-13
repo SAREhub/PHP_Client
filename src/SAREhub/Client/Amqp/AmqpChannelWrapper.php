84ffd078dc1ad43b50ada711a3df35840e58dbaa
Andrzej Wasiak <mararok@gmail.com>
2017-09-13 09:42:07 +0200

    fixed timeouts (#30)

--- src/SAREhub/Client/Amqp/AmqpChannelWrapper.php
+++ src/SAREhub/Client/Amqp/AmqpChannelWrapper.php
@@ -50,7 +50,12 @@ class AmqpChannelWrapper {
 			try {
 				$this->getChannel()->wait(null, true, $this->getWaitTimeout());
 			} catch (AMQPTimeoutException $e) {
-				sleep(1); // when queue is empty we can wait some time
+			    if ($e->getMessage() === "Timeout waiting on channel") {
+                    sleep(1); // when queue is empty we can wait some time
+                } else {
+			        throw $e; // other timeout - maybe connection lost
+                }
+
 			}
 		}
 	}
