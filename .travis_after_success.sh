4ca486405e999c71f5daab24923e12be1f2b0e5c
Andrzej Wasiak <a.wasiak@sarehub.com>
2017-10-26 12:41:01 +0200

    docs generate and line ending

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "SAREhub/PHP_Client" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "7.1" ]; then
+if [ "$TRAVIS_REPO_SLUG" = "SAREhub/PHP_Client" ] && [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_PHP_VERSION" = "7.1" ]; then
 
-    # Get ApiGen.phar
-    wget http://www.apigen.org/apigen.phar
-
-    # Generate Api
-    php apigen.phar generate -s src -d ../gh-pages --template-theme bootstrap
+    mkdir ../gh-pages
+    vendor/bin/apigen generate src -vv --destination ../gh-pages
     cd ../gh-pages
 
     # Set identity
+    echo "setting git identity"
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis"
 
     # Add branch
+    echo "initing git repo and creating branch"
     git init
     git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null
     git checkout -B gh-pages
 
     # Push generated files
+    echo "pushing generated docs"
     git add .
     git commit -m "APIGEN (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
     git push origin gh-pages -fq > /dev/null
