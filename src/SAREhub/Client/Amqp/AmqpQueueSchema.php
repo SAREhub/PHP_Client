915bbbd81565cfb45f052d19cfb19f7715463054
a.wasiak@sare.pl <a.wasiak@sare.pl>
2018-06-18 14:57:33 +0200

    refactored AmqpQueueSchema methods order

--- src/SAREhub/Client/Amqp/AmqpQueueSchema.php
+++ src/SAREhub/Client/Amqp/AmqpQueueSchema.php
@@ -38,63 +38,14 @@ class AmqpQueueSchema
      */
     private $arguments;
 
-    public static function newInstance(): self
-    {
-        return new self();
-    }
-
     public function __construct()
     {
         $this->arguments = new AMQPTable();
     }
 
-    public function getName(): string
+    public static function newInstance(): self
     {
-        return $this->name;
-    }
-
-    public function withName(string $name): AmqpQueueSchema
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function isPassive(): bool
-    {
-        return $this->passive;
-    }
-
-    public function withPassive(bool $passive): AmqpQueueSchema
-    {
-        $this->passive = $passive;
-        return $this;
-    }
-
-    public function isDurable(): bool
-    {
-        return $this->durable;
-    }
-
-    public function withDurable(bool $durable): AmqpQueueSchema
-    {
-        $this->durable = $durable;
-        return $this;
-    }
-
-    public function isExclusive(): bool
-    {
-        return $this->exclusive;
-    }
-
-    public function withExclusive(bool $exclusive): AmqpQueueSchema
-    {
-        $this->exclusive = $exclusive;
-        return $this;
-    }
-
-    public function isAutoDelete(): bool
-    {
-        return $this->autoDelete;
+        return new self();
     }
 
     public function withAutoDelete(bool $autoDelete): AmqpQueueSchema
@@ -103,14 +54,63 @@ class AmqpQueueSchema
         return $this;
     }
 
-    public function getArguments(): AMQPTable
-    {
-        return $this->arguments;
-    }
-
     public function withArguments(AMQPTable $arguments): AmqpQueueSchema
     {
         $this->arguments = $arguments;
         return $this;
+    }
+
+    public function withName(string $name): AmqpQueueSchema
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function withPassive(bool $passive): AmqpQueueSchema
+    {
+        $this->passive = $passive;
+        return $this;
+    }
+
+    public function withDurable(bool $durable): AmqpQueueSchema
+    {
+        $this->durable = $durable;
+        return $this;
+    }
+
+    public function withExclusive(bool $exclusive): AmqpQueueSchema
+    {
+        $this->exclusive = $exclusive;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function isPassive(): bool
+    {
+        return $this->passive;
+    }
+
+    public function isDurable(): bool
+    {
+        return $this->durable;
+    }
+
+    public function isExclusive(): bool
+    {
+        return $this->exclusive;
+    }
+
+    public function isAutoDelete(): bool
+    {
+        return $this->autoDelete;
+    }
+
+    public function getArguments(): AMQPTable
+    {
+        return $this->arguments;
     }
 }
